294f2f0f9b491130442af8f961ff898004e94d88
Ernest <musaubrian15@gmail.com>
2023-01-19 12:55:32 +0300

    chore: get data from redis cache

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Working with redis
+Module contains class definition of a redis client
 """
+from typing import Callable, Optional, Union
 import redis
 
 
@@ -12,6 +13,21 @@ class Cache:
     def __init__(self) -> None:
         """
         initialize the redis client
+        Attributes::
+            self._redis(redis.Redis): redis client
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
+
+    def get(self, key: str, func: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float, None]:
+        """
+        Return data from a redis Cache
+        Args::
+            key(str)
+            func(Optional[callable])
+        """
+        data = self._redis.get(key)
+        if data is not None and func is not None and callable(func):
+            return func(data)
+        return data

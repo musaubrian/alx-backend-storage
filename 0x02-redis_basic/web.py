c75c72a86d1b384217ea5a488b787776d9d7b6fb
Ernest <musaubrian15@gmail.com>
2023-01-19 13:46:49 +0300

    chore: track how many times a particular URL was accessed

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
+
+import redis
+import requests
+
+redis_client = redis.Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    """
+    get page and cache value
+    Args::
+        url(str)
+    """
+    redis_client.set(f"cached: {url}", count)
+    response = requests.get(url)
+    redis_client.incr(f"count:{url}")
+    redis_client.setex(f"cached: {url}", 10, redis_client.get(f"cached: {url}"))
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page(" http://slowwly.robertomurray.co.uk")
